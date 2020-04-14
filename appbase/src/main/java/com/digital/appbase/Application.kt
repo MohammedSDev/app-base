a897cef9465ef42562e7a37675b698ce9576fdda
Mohammed Salem <gg.goo.game@gmail.com>
2020-04-14 19:02:08 +0300

    add applyLanguageToBaseContext to AppAppBase.

--- appbase/src/main/java/com/digital/appbase/Application.kt
+++ appbase/src/main/java/com/digital/appbase/Application.kt
@@ -10,16 +10,21 @@ import com.digital.appktx.changeAppLocale
  * */
 abstract class AppAppBase : Application() {
 
+	fun applyLanguageToBaseContext(): Boolean = true
 
 	override fun attachBaseContext(base: Context?) {
 		val langCode = getUserLanguage(base)
-		super.attachBaseContext(changeAppLocale(langCode, base, {}))
+		if (applyLanguageToBaseContext())
+			super.attachBaseContext(changeAppLocale(langCode, base, {}))
+		else
+			super.attachBaseContext(base)
 	}
+
 	/**
 	 * return default or runtime app language code.{e.g: ar,en,fr,..ext}
 	 * snipe  e.g: if (@param context != null) getSavedUserLanguage() else Locale.getDefault().language
 	 * */
-	abstract fun getUserLanguage(context: Context?):String
+	abstract fun getUserLanguage(context: Context?): String
 
 
 	override fun onCreate() {
@@ -30,8 +35,8 @@ abstract class AppAppBase : Application() {
 	/**
 	 * return instance of your view model class.
 	 * e,g: modelClass.isAssignableFrom(YourViewModel::class.java) -> {
-					return YourViewModel(application, Repository())
-					}
+	return YourViewModel(application, Repository())
+	}
 	 * */
-	abstract fun <T: ViewModel?>createVM(modelClass: Class<T>):ViewModel
+	abstract fun <T : ViewModel?> createVM(modelClass: Class<T>): ViewModel
 }
