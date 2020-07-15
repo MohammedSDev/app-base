5916e55523d40b455f0a6da15ddeb7f0ed8a0b37
Mohammed Salem <gg.goo.game@gmail.com>
2020-07-15 19:22:22 +0300

    modify.

--- app/src/main/java/com/digital/appbasedemo/MainBaseActivity.kt
+++ app/src/main/java/com/digital/appbasedemo/MainBaseActivity.kt
@@ -13,5 +13,7 @@ class MainBaseActivity : AppBaseActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		supportFragmentManager.beginTransaction().add(R.id.root,TestFrag(),"gg")
+			.commit()
 	}
 }
